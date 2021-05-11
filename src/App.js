360a9d4123507ce184744f9ba0be5cf5ecd98e69
marcoweb <marcocarvalho.web@gmail.com>
2021-05-11 02:04:06 +0000

    Auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from './firebase'
+import { useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(null)
+
+    async function loginGoogle() {
+        const provedor = new firebase.auth.GoogleAuthProvider()
+        let resultado = await firebase.auth().signInWithPopup(provedor)
+        if(resultado) {
+            let novousuario = {
+                id : resultado.user.id,
+                name : resultado.user.displayName
+            }
+
+            setUser(novousuario)
+        } else {
+            alert('Erro')
+        }
+    }
+
+    if(user == null) {
+        return (
+            <div className="App">
+            <h1>Login</h1>
+            <button type="button" onClick={loginGoogle}>Login Google</button>
+            </div>
+        );
+    } {
+        return (
+            <div className="App">
+                <h1>Logado</h1>
+                <h3>Usuário: {user.name}</h3>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfcSvU9oG4zGQ1ezILTHzcWiJHg3kqGm0",
+  authDomain: "fir-react-exemplo-9db34.firebaseapp.com",
+  projectId: "fir-react-exemplo-9db34",
+  storageBucket: "fir-react-exemplo-9db34.appspot.com",
+  messagingSenderId: "859939516052",
+  appId: "1:859939516052:web:8fedc6049e7eca1b394e81"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
